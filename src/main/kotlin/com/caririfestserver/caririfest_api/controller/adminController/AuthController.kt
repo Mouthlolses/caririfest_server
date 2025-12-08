4a21373765860fd9da2f64605c9c227bf51043c0
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-12-08 15:44:58 -0300

    redeploy

--- src/main/kotlin/com/caririfestserver/caririfest_api/controller/adminController/AuthController.kt
+++ src/main/kotlin/com/caririfestserver/caririfest_api/controller/adminController/AuthController.kt
@@ -14,6 +14,7 @@ class AuthController(private val adminService: AdminService) {
 
     @PostMapping("/login")
     fun login(@RequestBody request: LoginRequest): LoginResponse {
+        println("JWT SECRET = ${System.getenv("JWT_SECRET")}")
         return adminService.login(request)
     }
 }
